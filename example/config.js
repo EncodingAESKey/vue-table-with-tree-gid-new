d6ce4025c07d42b24bbebf9998164bda1217fc23
wangwenli4 <wangwenli4@xdf.cn>
2022-02-10 11:01:05 +0800

    del: 删除冗余数据

--- example/config.js
+++ example/config.js
@@ -32,32 +32,24 @@ export const dataList = [
     {
         name: "/api/v2/tickets",
         serviceName: "api",
-        likes: ["football", "basketball"],
-        score: 10,
         startTime: 0,
         duration: 1900,
         children: [
             {
                 name: "tickets",
                 serviceName: "api",
-                likes: ["football", "basketball"],
-                score: 10,
                 startTime: 100,
                 duration: 200,
                 children: [
                     {
                         name: "query",
                         serviceName: "feapi",
-                        likes: ["football", "basketball"],
-                        score: 20,
                         startTime: 100,
                         duration: 300,
                     },
                     {
                         name: "checkFraud",
                         serviceName: "api",
-                        likes: ["football", "basketball"],
-                        score: 10,
                         startTime: 100,
                         duration: 400,
                     },
@@ -66,40 +58,30 @@ export const dataList = [
             {
                 name: "checkTateLimits",
                 serviceName: "api",
-                likes: ["football", "basketball"],
-                score: 20,
                 startTime: 0,
                 duration: 8.200,
             },
             {
                 name: "checkTateLimits",
                 serviceName: "api",
-                likes: ["football", "basketball"],
-                score: 20,
                 startTime: 8.200,
                 duration: 12.40,
             },
             {
                 name: "checkTateLimits",
                 serviceName: "api",
-                likes: ["football", "basketball"],
-                score: 20,
                 startTime: 12.40,
                 duration: 309.6,
                 children: [
                     {
                         name: "query",
                         serviceName: "feapi",
-                        likes: ["football", "basketball"],
-                        score: 20,
                         startTime: 300,
                         duration: 310,
                     },
                     {
                         name: "checkFraud",
                         serviceName: "api",
-                        likes: ["football", "basketball"],
-                        score: 10,
                         startTime: 310,
                         duration: 330,
                     },
